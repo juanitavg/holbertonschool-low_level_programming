d4f2eca2927c977d1085838bae44eddec2c03f0c
juanitavg <juanita.velasquez.g@gmail.com>
2025-01-08 05:16:06 +0000

    the final test

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -6,27 +6,29 @@
  * main - Entry point
  *
  * Description: Assign a random number to the variable n and
- * print whether the number is positive, negative, or zero.
+ * Prints if the number is positive, negative, or cero.
  *
  * Return: 0 (Always success)
  */
 
 int main(void)
 {
-        int n;
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        if (n > 0)
-        {
-                printf("%d is positive\n", n);
-        }
-        else if (n == 0)
-        {
-                printf("%d is zero\n", n);
-        }
-        else
-        {
-                printf("%d is negative\n", n);
-        }
-        return (0);
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+ 
+return (0);
+
 }
