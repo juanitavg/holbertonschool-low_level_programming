c638cad60641744d7ba58858e52bd4cd96bd7d3e
juanitavg <juanita.velasquez.g@gmail.com>
2025-01-08 04:00:41 +0000

    tetsing 4 puts

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+/**
+
+ * main - Entry point
+
+ *
+
+ * Description: Prints a specific message using printf
+
+ * Return: Always 0 (Success)
+
+ */
+
 int main(void)
+
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+
 }
