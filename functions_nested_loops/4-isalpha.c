c68269a43485e2c211d33fcb98ebc53a996bca78
juanitavg <juanita.velasquez.g@gmail.com>
2025-01-10 06:14:16 +0000

    A function that checks for alphabetic character

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,10 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);  /* Add this line */
-
-#endif /* MAIN_H */
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        return (1);
+    else
+        return (0);
+}
