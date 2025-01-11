67b3214d12624a550db61a88d2e3b4ae55efec32
juanitavg <juanita.velasquez.g@gmail.com>
2025-01-11 21:06:32 +0000

    A function that prints the 9 times table, starting with 0

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,41 +1,42 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table
- *
- * Return: void
- */
+* times_table - prints the 9 times table
+*
+* Return: void
+*/
 void times_table(void)
 {
-	int row, col, result;
+int row, col, result;
 
-	for (row = 0; row < 10; row++)
-	{
-		for (col = 0; col < 10; col++)
-		{
-			result = row * col;
+for (row = 0; row < 10; row++)
+{
+for (col = 0; col < 10; col++)
+{
+result = row * col;
 
-			if (col != 0)
-			{
-				_putchar(' ');
-			}
-
-			if (result < 10)
-			{
-				_putchar(result + '0');
-			}
-			else
-			{
-				_putchar(result / 10 + '0');  /* Print the tens place */
-				_putchar(result % 10 + '0');  /* Print the ones place */
-			}
-
-			if (col != 9) /* Avoid the comma after the last number in the row */
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
+if (col != 0)  /* Add a space bf every num except the first one */
+{
+_putchar(' ');
 }
+
+if (result < 10)
+{
+_putchar(result + '0');
+}
+else
+{
+_putchar(result / 10 + '0');  /* Print the tens place */
+_putchar(result % 10 + '0');  /* Print the ones place */
+}
+
+if (col != 9)  /* Avoid comma space af the last num in the row */
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
+
