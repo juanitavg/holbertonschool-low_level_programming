a604c7697ba8b8610e1f032e927d9563cbb2f916
juanitavg <juanita.velasquez.g@gmail.com>
2025-01-14 05:19:24 +0000

    A function that checks for uppercase character

--- more_functions_nested_loops/5-main.c
+++ more_functions_nested_loops/5-main.c
@@ -1,12 +1,12 @@
-#include "main.h"
+	#include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    more_numbers();
-    return (0);
-}
+	/**
+	* main - check the code
+	*
+	* Return: Always 0.
+	*/
+	int main(void)
+	{
+	more_numbers();
+	return (0);
+	}
