b37096abb476f572b9f7d85cfea7540fee331a89
juanitavg <juanita.velasquez.g@gmail.com>
2025-01-17 06:09:43 +0000

    Prototype

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,22 +1,30 @@
-	#include "main.h"
+#include "main.h"
 
-	/**
-	* print_triangle - prints a triangle
-	* @size: size of the triangle
-	*/
-	void print_triangle(int size)
-	{
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
 	if (size <= 0)
-	_putchar('\n');
+	{
+		_putchar('\n');
+	}
 	else
 	{
-	for (int i = 1; i <= size; i++)
-	{
-	for (int j = 1; j <= size - i; j++)
-	_putchar(' ');
-	for (int j = 1; j <= i; j++)
-	_putchar('#');
-	_putchar('\n');
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
-	}
-	}
+}
