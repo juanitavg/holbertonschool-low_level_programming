adbde89fd3d8d00079d8158d3a9a2a23244d83c3
juanitavg <juanita.velasquez.g@gmail.com>
2025-01-17 02:32:39 +0000

    A function that checks for a digit (0 through 9)

--- more_functions_nested_loops/1-main.c
+++ more_functions_nested_loops/1-main.c
@@ -1,13 +1,14 @@
-	#include "main.h"
-	#include <stdio.h>
+#include "main.h"
+#include "stdio.h"
 
-	/**
-	* main - check the code
-	*
-	* Return: Always 0.
-	*/
-	int main(void)
-	{
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
 	char c;
 
 	c = '0';
@@ -15,4 +16,4 @@
 	c = 'a';
 	printf("%c: %d\n", c, _isdigit(c));
 	return (0);
-	}
+}
