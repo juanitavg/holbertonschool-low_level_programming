0e7740dbe0f4b1a716f65d4853886d1186e54d78
juanitavg <juanita.velasquez.g@gmail.com>
2025-01-17 04:20:25 +0000

    A function that prints the numbers, from 0 to 9, followed by a new line

--- more_functions_nested_loops/4-main.c
+++ more_functions_nested_loops/4-main.c
@@ -1,12 +1,13 @@
-	#include "main.h"
+#include "main.h"
 
-	/**
-	* main - check the code
-	*
-	* Return: Always 0.
-	*/
-	int main(void)
-	{
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
 	print_most_numbers();
 	return (0);
-	}
+}
